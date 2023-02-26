9b2cc0205a4d51e9b3d81afdd2379461b078d209
sepler <sepler@users.noreply.github.com>
2023-02-26 17:09:38 -0500

    Update upload.js script to poll for IN_VAULT

--- scripts/upload.js
+++ scripts/upload.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const axios = require('axios');
 
 let upload_file_name = '';
-let get_url_endpoint = '';
+let api_endpoint = '';
 
 if (fs.existsSync('scripts/config.json')) {
     const script_config = JSON.parse(fs.readFileSync('scripts/config.json'));
     upload_file_name = script_config.upload_file_name;
-    get_url_endpoint = script_config.get_url_endpoint;
+    api_endpoint = script_config.api_endpoint;
 }
 
 (async () => {
     console.log('Reading blob');
     const blob = fs.readFileSync(upload_file_name)
     console.log('Read blob');
-    var res = await axios.get(get_url_endpoint);
+    var res = await axios.get(api_endpoint + '/getUploadUrl');
     console.log(res.data);
     const id = res.data.id;
     const uploadUrl = res.data.uploadUrl;
@@ -23,5 +23,21 @@ if (fs.existsSync('scripts/config.json')) {
             'Content-Disposition': `attachment; filename="${upload_file_name}"`,
             'Content-Type': 'binary/octet-stream'
         }
-    })
+    });
+
+    var poll = true;
+    while (poll) {
+        res = await axios.post(api_endpoint + '/getVaultItem', {id: id})
+        console.log(JSON.stringify(res.data));
+        if (res.data.status == 'IN_VAULT') {
+            break;
+        }
+        await sleep(1000);
+    }
 })();
+
+async function sleep(ms) {
+    return new Promise(resolve => {
+      setTimeout(resolve, ms);
+    });
+}
